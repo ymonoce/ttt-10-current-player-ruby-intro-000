56fd4d6fde3d0ba601b4f22acdf1ceaeb7c94f8e
Chloe Atwood <atwoodksw@gmail.com>
2020-12-09 02:59:23 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,3 +1,20 @@
+def position_taken? (board, index)
+
+# index number on board array is equal to X or O this will return true, anything else will return false
+  board[index] == "X" || board[index] == "O"
+end
+
 def turn_count(board)
+  
+counter = 0
+
+board.each do |xo| 
+  if xo == "X" || xo == "O"
+    counter += 1
+  
+end
+  
+  
+
 
 end
