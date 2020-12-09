33d437379bfb55cd797b2fce2077804448ef7e37
Chloe Atwood <atwoodksw@gmail.com>
2020-12-09 03:30:49 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,10 +6,10 @@ counter = 0
 # moves through each element of the board array and sets the local variable xo equal to their value as it goes
 board.each do |xo|
 # if each element of the board array "xo" is equal to an "X" or an "O" ...
-  if xo == "X" || xo == "O"
+  xo == "X" || xo == "O" ? counter += 1
 # ... increment counter by 1
     counter += 1
-    end
+    
 
   end
 # call "counter" variable in order to set the return value of turn_count to the value of "counter"
