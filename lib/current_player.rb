925311d42c3b81063b4a74fcbf2666563338115f
Chloe Atwood <atwoodksw@gmail.com>
2020-12-09 03:44:57 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -6,7 +6,7 @@ counter = 0
 # moves through each element of the board array and sets the local variable xo equal to their value as it goes
 board.each do |xo|
 # if each element of the board array "xo" is equal to an "X" or an "O" ...
-  if xo == "X" || xo == "O"
+    if xo == "X" || xo == "O"
 # ... increment counter by 1
     counter += 1
     end
