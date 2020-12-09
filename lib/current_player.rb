6d42f17a49937e20b63c202e2521985c2c520096
Chloe Atwood <atwoodksw@gmail.com>
2020-12-09 03:22:04 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,12 +1,22 @@
-def turn_count(board)
 
+# define method with one parameter "board"
+def turn_count (board)
+# create variable "counter" equal to zero
 counter = 0
-
+# moves through each element of the board array and sets the local variable xo equal to their value as it goes
 board.each do |xo|
+# if each element of the board array "xo" is equal to an "X" or an "O" ...
   if xo == "X" || xo == "O"
+# ... increment counter by 1
     counter += 1
     end
 
   end
+# call "counter" variable in order to set the return value of turn_count to the value of "counter"
 counter
+end
+
+# define method 
+def current_player (board)
+  
 end
